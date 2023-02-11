7c561f38c606eec05105882d47bb85d8a4eeee76
Senvlin_ <zjh13314339195@126.com>
2023-02-11 08:23:00 +0800

    issue1和issue2一起交上去了

--- src/CBot-API.py
+++ src/CBot-API.py
@@ -6,44 +6,67 @@ import asyncio
 import urllib.request
 import zlib
 
-#注意:由于本人的技术原因 暂时只支持WebSocket
+# 注意:由于本人的技术原因 暂时只支持WebSocket
 
 
-async def useAPI(addr,Token,TimeOut=6):#访问kook api的方法,自动将返回的Json转为字典 需要提供地址（如/api/v3/gateway/index 为Gateway的地址）和Token
-    return json.loads(urllib.request.urlopen(urllib.request.Request("https://www.kookapp.cn/"+addr,headers={'Authorization': 'Bot '+Token}),timeout=TimeOut).read())
+async def useAPI(
+    addr, Token, TimeOut=6
+):  # 访问kook api的方法,自动将返回的Json转为字典 需要提供地址（如/api/v3/gateway/index 为Gateway的地址）和Token
+    return json.loads(
+        urllib.request.urlopen(
+            urllib.request.Request(
+                "https://www.kookapp.cn/" + addr,
+                headers={'Authorization': 'Bot ' + Token},
+            ),
+            timeout=TimeOut,
+        ).read()
+    )
 
-async def GetWay(Token,compress=1):
-    index=1
+
+async def GetWay(Token, compress=1):
+    index = 1
 
     while True:
-        WaitTime=2
+        WaitTime = 2
         try:
-            return (await asyncio.create_task(useAPI("/api/v3/gateway/index?compress="+str(compress), Token)))["data"]["url"] #调用useAPI获取Gateway地址
-        except Exception as e:#出现异常时自动开始指数回退（参考PHP-Bot）
-            print("CBot>",str(e))
-            if WaitTime>=60:
-                WaitTime=60
+            return (
+                await asyncio.create_task(
+                    useAPI(
+                        "/api/v3/gateway/index?compress="
+                        + str(compress),
+                        Token,
+                    )
+                )
+            )["data"][
+                "url"
+            ]  # 调用useAPI获取Gateway地址
+        except Exception as e:  # 出现异常时自动开始指数回退（参考PHP-Bot）
+            print("CBot>", str(e))
+            if WaitTime >= 60:
+                WaitTime = 60
             else:
-                WaitTime**=index
-                index+=1
-            print("CBot> Wating",WaitTime,"s......")
+                WaitTime **= index
+                index += 1
+            print("CBot> Wating", WaitTime, "s......")
             await asyncio.sleep(WaitTime)
-                
+
             continue
 
-async def Connect(url):#用于连接 返回已经连接的Websocket，提供WateGate
+
+async def Connect(url):  # 用于连接 返回已经连接的Websocket，提供WateGate
     return await websockets.connect(url)
 
-async def Receive(websocket,compress=1):#这里提供Websocket
-    #访问Gateway 并接收报文
-    if compress==0:
-        data=await websocket.recv()
+
+async def Receive(websocket, compress=1):  # 这里提供Websocket
+    # 访问Gateway 并接收报文
+    if compress == 0:
+        data = await websocket.recv()
     else:
-        data=await zlib.decompress(await websocket.recv())#用于解压报文
+        data = zlib.decompress(await websocket.recv())  # 用于解压报文
     JsData = json.loads(data)
-    Code = JsData["d"]["code"] #提取状态码 用于发起异常
-    if JsData["s"]==1 or JsData["s"]==5:
-        if Code!=0:#后面用来发起异常的 方便使用者根据实际情况调整
+    Code = JsData["d"]["code"]  # 提取状态码 用于发起异常
+    if JsData["s"] == 1 or JsData["s"] == 5:
+        if Code != 0:  # 后面用来发起异常的 方便使用者根据实际情况调整
             if Code == 40100:
                 raise ValueError["Missing Parameter!"]
             if Code == 40101:
@@ -51,26 +74,35 @@ async def Receive(websocket,compress=1):#这里提供Websocket
             if Code == 40102:
                 raise ValueError["Token Validation Failed!"]
             if Code == 40103:
-                 raise ValueError["Token Expired! Please GateWay Again!"]
+                raise ValueError[
+                    "Token Expired! Please GateWay Again!"
+                ]
             if Code == 40106:
                 raise ValueError["Missing Parameter! RECONNECT!!!"]
             if Code == 40107:
-                raise ValueError["Current SessionHas Expired! RECONNECT!!!"]
+                raise ValueError[
+                    "Current SessionHas Expired! RECONNECT!!!"
+                ]
             if Code == 40108:
                 raise ValueError["Invalid sn! RECONNECT!!!"]
     return JsData
 
-async def Sent(websocket,data):
+
+async def Sent(websocket, data):
     websocket.Sent(json.dumps(data))
 
-async def Ping(websocket,sn):
+
+async def Ping(websocket, sn):
     ping = {"s": 2}
     ping["sn"] = sn
-    await Sent(websocket,ping)
+    await Sent(websocket, ping)
 
-async def Retriever(websocket,ReturnList,ConditionList,compress=1):#检索器
+
+async def Retriever(
+    websocket, ReturnList, ConditionList, compress=1
+):  # 检索器
     while True:
-        ReturnList.append(await Receive(websocket,compress))
+        ReturnList.append(await Receive(websocket, compress))
         for Judge in ConditionList:
             try:
                 for Condition in Judge:
@@ -83,23 +115,17 @@ async def Retriever(websocket,ReturnList,ConditionList,compress=1):#检索器
                 pass
 
 
-
-
-if __name__ == "__main__":#测试代码
+async def main():
+    '''程序入口'''
     print("CBot> GateWay")
-    ws = asyncio.run(Connect(asyncio.run(GetWay("<Token>"))))
-
-    print("CBot> wating for HELLO")
-
     l = []
+    url = await GetWay("1/MTUzMzQ=/Fx08J7b32HGVL1Lq/4ELnw==")
+    print("CBot> wating for HELLO")
+    async with websockets.connect(url) as ws:
+        ret = await Retriever(ws, l, [[["s", 1]]])
+        print("CBot> ", ret)
+        sn = 0
 
 
-    ret =asyncio.run(asyncio.wait_for(asyncio.run(Retriever(ws,l,[[["s",1]]])),6))
-    print("CBot> ",ret)
-    sn=0
-
-
-
-
-
-
+if __name__ == "__main__":  # 测试代码
+    asyncio.run(main())
